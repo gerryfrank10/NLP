8c5b3f2e8ebe7245b0e60fde5cb2129986a309fa
gerald <amasi.gerald@icloud>
2025-07-21 21:15:32 +0100

    NER

--- cyber/github-md.py
+++ cyber/github-md.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 # Combine all .md files into one corpus
 corpus = []
-for md_file in Path("cyber-apocalypse-2024").rglob("*.md"):
+for md_file in Path("ctf-wiki-en").rglob("*.md"):
     corpus.append(md_file.read_text())
 
-with open("cyber-apocalypse-2024.txt", "w") as f:
+with open("scraped_pages/ctf-wiki.txt", "w") as f:
     f.write("\n\n".join(corpus))  # Separate files by double newlines
